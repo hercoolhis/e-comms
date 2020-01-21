362d0122ce283127198ee6c7834b5746ae4716d9
Sulaiman Agbenla <agbenla.lanre@gmail.com>
2020-01-21 02:18:30 +0100

    multiple product to cart

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,17 @@
+export const addItemToCart = (cartItems, itemToAdd) => {
+    const existingItem = cartItems.find(cartItem => cartItem.id === itemToAdd.id);
+
+    if (existingItem) {
+        return cartItems.map(
+            cartItem => {
+                if (cartItem.id === itemToAdd.id) {
+                    return { ...cartItem, quantity: cartItem.quantity + 1 };
+                } else {
+                    return cartItem;
+                }
+            }
+        )
+    }
+
+    return [...cartItems, { ...itemToAdd, quantity: 1}];
+}
